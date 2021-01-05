f0b8252f5ad31fbbdd87a9a650c7952bac3bed52
Robert Obi <robobi97@googlemail.com>
2021-01-05 01:39:50 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,1 +1,7 @@
-` require 'pry' def start_game(player1, player2)   "Hello #{player1} & #{player2}" end  def play_game(player1, player2)   "#{player1} is better than #{player2}" end
+require ‘pry’
+def start_game(player1, player2)
+  “Hello #{player1} & #{player2}”
+end
+def play_game(player1, player2)
+  “#{player1} is better than #{player2}”
+end
